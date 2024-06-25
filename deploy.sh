f59afa60688819fb52facaadadc7e233311459c1
Ilya <ilyakozorezov@gmail.com>
2024-06-25 14:14:16 -0700

    server only killed if exists

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,9 @@
-kill $(lsof -ti :80)
+if 
+process = $(lsof -ti :80 2>/dev/null)
+if [[ ! -z "$process" ]]; then
+    echo "Killing previous server"
+    kill -9 $process
+else
+    echo "No server to kill"
+
 JENKINS_NODE_COOKIE=dontKillMe ./Jenkins &
