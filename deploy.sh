6cf70f3f17f799eca3334ce351fd99db7b90884f
Ilya <ilyakozorezov@gmail.com>
2024-06-25 14:25:07 -0700

    idc anymore'

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-process=$(lsof -ti :80)
+lsof -ti :80
 status=$(echo $?)
 if [[ "$status"=="0" ]]; then
     echo "Killing previous server"
-    kill -9 $process
+    kill -9 $(lsof -ti :80)
 else
     echo "No server to kill"
 
